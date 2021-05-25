87122a8b84bc5ff764962e4e23a5a0bacf0486f5
Mickael Mignard <mignard.mickael@gmail.com>
2021-05-25 09:13:16 +0200

    Mise au propre du code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+// Get input element
 let inputLastName = document.getElementById("lastname");
 
-inputLastName.addEventListener("keypress", function(){
-   alert(inputLastName.value)  
+// Add on input an event listener on keyup to display the character typed
+inputLastName.addEventListener("keyup", function(){
+   alert(inputLastName.value);  
 })
